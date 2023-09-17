90979d3ab274aaf7cf9e00542c72274c4bdbaf20
Eno Ciraku <eno.ciraku93@gmail.com>
2023-09-17 15:09:23 +0200

    Try to get all data. Error in season 0304

--- SerieAPipeline.py
+++ SerieAPipeline.py
@@ -18,3 +18,35 @@ juventusHomeWinningData = juventusData[(juventusData["HomeTeam"] == 'Juventus') 
 juventusAwayWinningData = juventusData[(juventusData["AwayTeam"] == 'Juventus') & (juventusData["FTR"] == 'A')]
 
 juventusWinningData = juventusData[((juventusData["HomeTeam"] == 'Juventus') & (juventusData["FTR"] == 'H')) | ((juventusData["AwayTeam"] == 'Juventus') & (juventusData["FTR"] == 'A'))]
+
+# Football season is expressed as a combination of 2 years, for example: season 91-92. In the website the hyphen 
+# is missing so it is: 9192. This is how we are goning to loop over the files to extract all seasons.
+
+# First year in the football season
+year1 = 0
+# Second year in the football season
+year2 = 1
+
+for index in range(1, 101):
+    
+    # Convert the years from integer to character (so that you can add the 0s later):
+    year1_str = str(year1)
+    year2_str = str(year2)
+    
+    if year1 < 10:
+        year1_str = "0" + year1_str # Add extra 0 in front of the year number
+    if year2 < 10:
+        year2_str = "0" + year2_str # Add extra 0 in front of the year number
+        
+    file_url = 'https://www.football-data.co.uk/mmz4281/{}/I1.csv'.format(year1_str + year2_str)
+    print(year1_str, year2_str)
+    print(file_url)
+    if requests.get(file_url).status_code == 200:
+        urlData = requests.get(file_url).content
+        rawData = pd.read_csv(io.StringIO(urlData.decode('utf-8')), names=["Div", "HomeTeam", "AwayTeam", "FTHG", "FTAG", "FTR"])
+        cleanData = rawData[["Div", "HomeTeam", "AwayTeam", "FTHG", "FTAG", "FTR"]].dropna()
+        print(cleanData)
+        
+    
+    year1 = (year1 + 1) % 100
+    year2 = (year2 + 1) % 100
